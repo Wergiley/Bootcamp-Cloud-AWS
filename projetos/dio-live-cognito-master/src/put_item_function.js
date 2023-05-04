015efe96612a2862ba9a0f5182d161630bcfd12e
Wergiley Oliveira <72641269+Wergiley@users.noreply.github.com>
2023-05-03 21:43:30 -0300

    Update put_item_function.js

--- projetos/dio-live-cognito-master/src/put_item_function.js
+++ projetos/dio-live-cognito-master/src/put_item_function.js
@@ -10,7 +10,6 @@ exports.handler = async (event) => {
     
     const params = {
       TableName : 'Items',
-      /* Item properties will depend on your application concerns */
       Item: {
          id: id,
          price: price
@@ -36,4 +35,5 @@ exports.handler = async (event) => {
     };
     
     return response;
+    // Oi, ^_^
 };
